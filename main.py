0f8f669104239f4f7b908119ca08f0c9b84c3459
Jack Wetmore <jackwwetmore@gmail.com>
2026-07-10 19:06:39 -0700

    displays if the space bar was single double triple...... pressed now

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 from pynput import keyboard
+import time
+
 control = keyboard.Controller()
 Key = keyboard.Key
 ignore = False
+spacetimes = []
+
 def pressed(key):
     global ignore
-    print(key)
-    if key == keyboard.KeyCode.from_char('q'):
-        print("space pressed")
-        ignore = True
-        control.press(Key.space)
-        control.release(Key.space)
-        ignore = False
+    #print(key)
 def released(key):
     """if key == Key.space:
         print("key released")
         control.release(key)"""
 def win32_event_filter(msg, data):
     global ignore
-    INJECTED = 0x10
-    print(data.flags)
-    print(msg, data.vkCode)
+    #print(data.flags)
+    #print(msg, data.vkCode)
     if data.vkCode == 32 and not ignore:
-        print("space blocked")
+        if msg != 257:
+            spacetimes.append(time.time())
+        #print("space blocked")
         listener.suppress_event()
-with keyboard.Listener(on_press=pressed,on_release=released,win32_event_filter=win32_event_filter) as listener:
-    listener.join()
+listener = keyboard.Listener(on_press=pressed,on_release=released,win32_event_filter=win32_event_filter)
+listener.start()
+times = 0
+while True:
+    if len(spacetimes) > 0:
+        times = time.time() - spacetimes[-1]
+    if times > 0.3:
+        print(len(spacetimes))
+        spacetimes.clear()
+        times = 0
